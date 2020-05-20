de327f75cd62c3359e4a5e6e6ae7f27a697d6117
Joao Pedro <jp.ssilva@live.com>
2020-05-20 09:13:24 -0300

    Atributos

---
+++ MeuAtributo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_Atributos
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
+    class MeuAtributo : Attribute 
+    {
+
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public MeuAtributo(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_Atributos
+{
+    [MeuAtributo("Atributo Classe", Descricao ="Descrição do atributo")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
